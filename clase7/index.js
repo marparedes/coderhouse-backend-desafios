a142d06d2d7504aa882b4c233e10cc0166f51398
marparedes <m-ariana_98@hotmail.com>
2022-06-15 20:00:39 -0300

    agrego router

--- clase7/index.js
+++ clase7/index.js
@@ -3,18 +3,13 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8080
 
-const productsRoutes = require("./routes/productos")
-const randomRoute = require("./routes/productoRandom")
-
+const routes = require("./routes/productos")
 app.use("/static", express.static("./public"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.use("/productos", productsRoutes)
-app.use("/productoRandom", randomRoute)
-
-
+app.use("/api/productos", routes)
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
